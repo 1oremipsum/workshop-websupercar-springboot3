35616ace4f9acec38b09425e1146c900502c204b
Allan Sanches <allanterzisanches@gmail.com>
2024-05-25 17:32:22 -0300

    Project Created

---
+++ src/test/java/com/websupercar/supercar/SupercarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websupercar.supercar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
