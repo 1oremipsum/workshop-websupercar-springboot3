edfd01df9d50811132e0e774e16f8c4818ee7cc5
Allan Sanches <allanterzisanches@gmail.com>
2024-06-08 20:11:02 -0300

    Service layer, component registration

--- src/main/java/com/websupercar/supercar/resources/UserResource.java
+++ src/main/java/com/websupercar/supercar/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.websupercar.supercar.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.websupercar.supercar.entities.User;
+import com.websupercar.supercar.service.UserService;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> userRequestTest(){
-		return ResponseEntity.ok().body(new User
-				(1L, "Allan", "allan@gmail.com", "1grupochonps", "99 9999-9999"));
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User u = service.findById(id);
+		return ResponseEntity.ok().body(u);
 	}
 }
