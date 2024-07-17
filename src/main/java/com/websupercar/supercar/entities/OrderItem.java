ee26d45141227d46afe716a184770de532b1f3e1
Allan San <allanterzisanches@gmail.com>
2024-07-17 01:49:27 -0300

    Many-to-many association with extra data (embedded class)

--- src/main/java/com/websupercar/supercar/entities/OrderItem.java
+++ src/main/java/com/websupercar/supercar/entities/OrderItem.java
@@ -1,25 +1,50 @@
 package com.websupercar.supercar.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class OrderItem {
-	private Long id;
+import com.websupercar.supercar.entities.pk.OrderItemPk;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPk id;
 	private Integer quantity;
 	private Double price;
-	
-	public OrderItem() {}
 
-	public OrderItem(Long id, Integer quantity, Double price) {
-		this.id = id;
+    public OrderItem() {}
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
 	}
 
-	public Double subtotal() {
+    public Double subtotal() {
 		return quantity * price;
 	}
 
-	public Integer getQuantity() {
+    public Order getOrder(){
+        return id.getOrder();
+    }
+    public void setOrder(Order o){
+        id.setOrder(o);
+    }
+    public Product getProduct(){
+        return id.getProduct();
+    }
+    public void setProduct(Product p){
+        id.setProduct(p);
+    }
+    public Integer getQuantity() {
 		return quantity;
 	}
 	public void setQuantity(Integer quantity) {
@@ -31,17 +56,13 @@ public class OrderItem {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-	public Long getId() {
-		return id;
-	}
 
-	@Override
+    @Override
 	public String toString() {
 		return "OrderItem [id=" + id + ", quantity=" + quantity + 
 				", price=" + price + "]";
 	}
-
-	@Override
+    @Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
