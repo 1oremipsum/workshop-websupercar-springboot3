26a36b58f8e39b459caa6e9c8b393e1fd80e2f62
Allan San <allanterzisanches@gmail.com>
2024-07-29 19:00:02 -0300

    User update

--- src/main/java/com/websupercar/supercar/service/UserService.java
+++ src/main/java/com/websupercar/supercar/service/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id){
 		repository.deleteById(id);
 	}
+
+	public User update(Long id, User obj){
+		User entity = repository.getReferenceById(id);
+		setData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void setData(User entity, User obj){
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
