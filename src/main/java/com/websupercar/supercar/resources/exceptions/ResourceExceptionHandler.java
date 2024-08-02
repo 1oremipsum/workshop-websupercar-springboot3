d423eabcfce43ee7e66dc0afcdd8ad2a30a66626
Allan San <allanterzisanches@gmail.com>
2024-08-01 21:43:27 -0300

    Exception handling - delete

--- src/main/java/com/websupercar/supercar/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/websupercar/supercar/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.websupercar.supercar.service.exceptions.DatabaseException;
 import com.websupercar.supercar.service.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,15 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> rnf(ResourceNotFoundException e, HttpServletRequest request){
         String typeError = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        String message = e.getMessage();
+        StandardError error = new StandardError(Instant.now(), status.value(), typeError, message, request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> dbe(ResourceNotFoundException e, HttpServletRequest request){
+        String typeError = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         String message = e.getMessage();
         StandardError error = new StandardError(Instant.now(), status.value(), typeError, message, request.getRequestURI());
         return ResponseEntity.status(status).body(error);
